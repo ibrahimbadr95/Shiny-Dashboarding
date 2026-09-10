0cb7bb8eb3fc05e51e99a2a8721c3adabc82591a
ibrahimbadr95 <engheama@gmail.com>
2026-09-10 13:25:13 +0200

    shinydashboard library gebruikt

--- test shiny dashboard.R
+++ test shiny dashboard.R
@@ -1,41 +1,42 @@
 library(shiny)
-library(semantic.dashboard)
+library(shinydashboard)
 library(DT)
 
-ui <- dashboardPage( theme = "superhero",
-  dashboardHeader(title = "Test Dashboard"),
-  dashboardSidebar(
-    sidebarMenu(
-      menuItem("Iris", tabName = "iris", icon = icon("tree")),
-      menuItem("Cars", tabName = "cars", icon = icon("car"))
-    )
-  ),
-  dashboardBody(
-    tabItems(
-      tabItem(tabName = "iris",
-              box(plotOutput("correlation_plot"), width = 8),
-              box(
-                selectInput("features", "Features:",
-                            c("Sepal.Width", "Petal.Length", "Petal.Width")),
-                width = 4
-              )
-      ),
-      tabItem(tabName = "cars",
-              fluidPage(
-                h1("Cars"),
-                dataTableOutput("carstable")
-              )
-      )
-    )
-  )
+ui <- dashboardPage( skin = "red",
+                     dashboardHeader(title = "Testdashboard"),
+                     dashboardSidebar(
+                       sidebarMenu(
+                         menuItem("Iris", tabName = "iris", icon = icon("tree")),
+                         menuItem("Auto's", tabName = "cars", icon = icon("car"))
+                       )
+                     ),
+                     dashboardBody(
+                       tabItems(
+                         tabItem(tabName = "iris",
+                                 box(plotOutput("correlation_plot"), width = 8),
+                                 box(
+                                   selectInput("features", "Kenmerken:",
+                                               c("Kelkbladbreedte" = "Sepal.Width",
+                                                 "Kroonbladlengte" = "Petal.Length",
+                                                 "Kroonbladbreedte" = "Petal.Width")),
+                                   width = 4
+                                 )
+                         ),
+                         tabItem(tabName = "cars",
+                                 fluidPage(
+                                   h1("Auto's"),
+                                   dataTableOutput("carstable")
+                                 )
+                         )
+                       )
+                     )
 )
 
 server <- function(input, output){
   output$correlation_plot <- renderPlot({
-    plot(iris$Sepal.Length, iris[[input$features]], xlab= "Sepal Length", ylab = "Feature")
+    plot(iris$Sepal.Length, iris[[input$features]], xlab= "Kelkbladlengte", ylab = "Kenmerk")
   })
   output$carstable <- renderDataTable(mtcars)
 }
 
 shinyApp(ui, server)
-
